56d865f945a6fadfce4c88e5424a529f30cf1238
bagusindrayana <bagusindrayanaindo@gmail.com>
2024-03-09 20:41:46 +0800

    dark theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import NavMenu from "@/components/navmenu";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider"
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -14,29 +15,33 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  
+
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
 
-        <NavMenu></NavMenu>
 
-        {children}
 
-         {/* full width footer with tailwind */}
-        <footer className="bg-gray-800 text-white p-4 text-center">
-          <p>
-            Made with{" "}
-            <span role="img" aria-label="heart">
-              ❤️
-            </span>{" "}
-            by{" "}
-            <a
-              className="underline"
-              href="https://github.com/bagusindrayana/">Dev</a>
-          </p>
-        </footer>
-
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        ><NavMenu></NavMenu>
+          {children}
+          <footer className="bg-gray-800 text-white p-4 text-center">
+            <p>
+              Made with{" "}
+              <span role="img" aria-label="heart">
+                ❤️
+              </span>{" "}
+              by{" "}
+              <a
+                className="underline"
+                href="https://github.com/bagusindrayana/">Dev</a>
+            </p>
+          </footer>
+        </ThemeProvider>
       </body>
     </html>
   );
--- components/navmenu.tsx
+++ components/navmenu.tsx
@@ -1,5 +1,4 @@
 'use client'
-import { Inter } from "next/font/google";
 import { FaGithub } from "react-icons/fa";
 import { TiThMenu } from "react-icons/ti";
 import { useState } from "react";
@@ -13,7 +12,23 @@ import {
     MenubarTrigger,
 } from "@/components/ui/menubar"
 
+import * as React from "react"
+
+import { useTheme } from "next-themes"
+
+import { Button } from "@/components/ui/button"
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { BsMoonStarsFill } from "react-icons/bs";
+import { BsSunFill } from "react-icons/bs";
+import { Label } from "@/components/ui/label"
+
 export default function NavMenu() {
+    const { setTheme } = useTheme()
     const menus = {
         "/": "Home",
         "/scam-apk": "SpamScam APK",
@@ -30,7 +45,7 @@ export default function NavMenu() {
             </div>
 
             {/* responsive menu */}
-            <div className="hidden md:flex">
+            <div className="hidden md:flex items-center">
                 {Object.entries(menus).map(([href, label]) => (
                     <a
                         key={href}
@@ -40,9 +55,30 @@ export default function NavMenu() {
                         {label}
                     </a>
                 ))}
+                
                 <a className="mx-2" href="https://github.com/bagusindrayana/extract-apk-web">
                     <FaGithub className="text-white text-2xl" />
                 </a>
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button variant="outline" size="icon" id="changeThemeMobile">
+                            <BsSunFill className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                            <BsMoonStarsFill className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                            <span className="sr-only">Toggle theme</span>
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end">
+                        <DropdownMenuItem onClick={() => setTheme("light")}>
+                            Light
+                        </DropdownMenuItem>
+                        <DropdownMenuItem onClick={() => setTheme("dark")}>
+                            Dark
+                        </DropdownMenuItem>
+                        <DropdownMenuItem onClick={() => setTheme("system")}>
+                            System
+                        </DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
             </div>
 
             {/* hamburger menu for mobile */}
@@ -62,8 +98,33 @@ export default function NavMenu() {
                             <MenubarSeparator />
                             <MenubarItem>
                                 <a className="mx-1 flex" href="https://github.com/bagusindrayana/extract-apk-web">
-                                    <FaGithub className="text-2xl mr-1" /> Repository
+                                    <FaGithub className="text-2xl mx-2" /> <span>Repository</span>
                                 </a>
+                            </MenubarItem>
+                            <MenubarItem>
+                                <div className="flex mx-1 content-center items-center align-middle">
+                                    <DropdownMenu>
+                                        <DropdownMenuTrigger asChild>
+                                            <Button variant="outline" size="icon" id="changeThemeMobile">
+                                                <BsSunFill className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                                                <BsMoonStarsFill className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                                                <span className="sr-only">Toggle theme</span>
+                                            </Button>
+                                        </DropdownMenuTrigger>
+                                        <DropdownMenuContent align="end">
+                                            <DropdownMenuItem onClick={() => setTheme("light")}>
+                                                Light
+                                            </DropdownMenuItem>
+                                            <DropdownMenuItem onClick={() => setTheme("dark")}>
+                                                Dark
+                                            </DropdownMenuItem>
+                                            <DropdownMenuItem onClick={() => setTheme("system")}>
+                                                System
+                                            </DropdownMenuItem>
+                                        </DropdownMenuContent>
+                                    </DropdownMenu>
+                                    <Label className="ml-1" htmlFor="changeThemeMobile">Theme</Label>
+                                </div>
                             </MenubarItem>
                         </MenubarContent>
                     </MenubarMenu>
